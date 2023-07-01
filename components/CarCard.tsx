541b5db587a362709aa93e4712582174c20c6aaa
Johandika <johanelyosse@gmail.com>
2023-07-01 21:27:00 +0700

    Server side render done!

---
+++ components/CarCard.tsx
@@ -0,0 +1,74 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+
+import { CarProps } from "@/types";
+import CustomButton from "./CustomButton";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
+import CarDetails from "./CarDetails";
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { city_mpg, drive, make, model, transmission, year } = car;
+  const carRent = calculateCarRent(city_mpg, year);
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content car-card__content-title">
+        <h2>
+          {make}
+          {model}
+        </h2>
+      </div>
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold ">$</span>
+        {carRent}
+        <span className="self-end  text-[14px] font-medium">/hari</span>
+      </p>
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src={generateCarImageUrl(car)}
+          alt="car image"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
+
+      <div className="relative w-full flex mt-2 ">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/steering-wheel.svg" width={20} height={20} alt="steering wheel" />
+            <p>{transmission === "a" ? "Automatic" : "Manual"}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="steering wheel" />
+            <p>{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="steering wheel" />
+            <p> {city_mpg} MPG</p>
+          </div>
+        </div>
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="Lihat Detail"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
+      </div>
+
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
+    </div>
+  );
+};
+
+export default CarCard;
---
+++ utils/index.ts
@@ -0,0 +1,61 @@
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, fuel, model, year, limit } = filters;
+
+  const headers = {
+    "X-RapidAPI-Key": "3945351cedmshd65ff396a34543cp114bebjsnc575c0cdc39a",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Harga sewa dasar per hari dalam dolar
+  const mileageFactor = 0.1; // Tarif tambahan per mil yang ditempuh
+  const ageFactor = 0.05; // Tarif tambahan per tahun umur kendaraan
+
+  // Hitung tarif tambahan berdasarkan jarak tempuh dan usia
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Hitung total tarif sewa per hari
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+
+  const { make, year, model } = car;
+
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathName;
+};
